a642b9b6b754ee8501662465b5414a6ac8ea2165
inung1004 <dksdbswl10430@naver.com>
2023-10-02 22:45:43 +0900

    feat :: tab, header 컴포넌트 추가

---
+++ src/components/main/Header.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { Logo } from "../../assets";
+
+const Header = () => {
+  return (
+    <Container>
+      <img src={Logo} alt="로고 이미지" />
+      <Right>
+        <p>홍길동 님</p>
+        <LogOutButton>로그아웃</LogOutButton>
+      </Right>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 36px;
+  > p {
+    color: ${({ theme }) => theme.colors.gray900};
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 29px;
+  }
+`;
+
+const LogOutButton = styled.button`
+  width: 88px;
+  height: 40px;
+  border-radius: 8px;
+  border: none;
+  line-height: 21px;
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.gray700};
+  background: ${({ theme }) => theme.colors.gray100};
+`;
+
+export default Header;
---
+++ src/components/main/Tab.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { tabItem } from "../../constants/main";
+
+interface TabProps {
+  clickItem: string;
+  setClickItem: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Tab = ({ clickItem, setClickItem }: TabProps) => {
+  return (
+    <Container>
+      {tabItem.map((item, idx) => (
+        <Button
+          isClick={clickItem === item}
+          onClick={() => setClickItem(item)}
+          key={idx}
+        >
+          {item}
+        </Button>
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 72px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray100};
+  gap: 48px;
+`;
+
+const Button = styled.button<{ isClick: boolean }>`
+  height: inherit;
+  border: none;
+  background: none;
+  font-size: 20px;
+  font-weight: 600;
+  color: ${(props) =>
+    props.isClick ? props.theme.colors.primary : props.theme.colors.gray900};
+  border-bottom: ${(props) =>
+    props.isClick ? `2px solid ${props.theme.colors.primary}` : "none"};
+`;
+
+export default Tab;
