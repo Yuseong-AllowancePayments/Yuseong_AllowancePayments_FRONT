2c269f229df908aca8f30ca4f70632aa686c57a6
inung1004 <dksdbswl10430@naver.com>
2023-12-15 23:43:45 +0900

    feat :: DTO 생성

---
+++ src/models/request/index.ts
@@ -0,0 +1,89 @@
+export interface userLoginRequestDto {
+  accountId: string;
+  password: string;
+}
+
+export interface saveExcelRequestDto {
+  type: "WAR_VETERAN" | "WAR_VETERAN_SPOUSE" | "HONORABLE_ALLOWANCE";
+  file: File;
+}
+
+export interface editExcelTargetRequestDto {
+  excelLine: {
+    serial_number: number;
+    hang_jung_dong: string;
+    veterans_number: string;
+    resident_registration_number: string;
+    name: string;
+    address: string;
+    deposit_type: string;
+    bank_name: string;
+    account_holder: string;
+    bank_account_number: string;
+    sibi: string;
+    gubi: string;
+    allowance_type: string;
+    note: string;
+  };
+  id: number;
+}
+
+export interface editExcelCashRequestDto {
+  excelLine: {
+    serial_number: number;
+    hang_jung_dong: string;
+    veterans_number: string;
+    resident_registration_number: string;
+    name: string;
+    address: string;
+    deposit_type: string;
+    sibi: number;
+    gubi: number;
+    allowance_type: string;
+    note: string;
+  };
+  id: number;
+}
+
+export interface editExcelNewcomerRequestDto {
+  excelLine: {
+    serial_number: number;
+    hang_jung_dong: string;
+    veterans_number: string;
+    resident_registration_number: string;
+    name: string;
+    address: string;
+    deposit_type: string;
+    bank_name: string;
+    account_holder: string;
+    bank_account_number: string;
+    allowance_type: string;
+    note: string;
+    transfer_reason: string;
+    transfer_date: string;
+  };
+  id: number;
+}
+
+export interface editExcelStoppedRequestDto {
+  excelLine: {
+    serial_number: number;
+    hang_jung_dong: string;
+    veterans_number: string;
+    resident_registration_number: string;
+    name: string;
+    address: string;
+    deposit_type: string;
+    bank_name: string;
+    account_holder: string;
+    bank_account_number: string;
+    sibi: string;
+    gubi: string;
+    allowance_type: string;
+    note: string;
+    stopped_reason: string;
+    stopped_date: string;
+    transfer_address: string;
+  };
+  id: number;
+}
